1c76dfeee9dd1dde79023d012678e2006a83e185
blackSooooo <507pdophs@naver.com>
2021-06-03 13:05:38 +0900

    feat: layout change

--- src/screens/Account/SettingPage.js
+++ src/screens/Account/SettingPage.js
@@ -12,50 +12,52 @@ const SettingPage = () => {
     const { signout } = useContext(AuthContext);
     const {deletenoticetoken} = useContext(NoticeContext);
     return (
-        <ScrollView style={{flex: 1, backgroundColor: 'rgb(254,254,254)', paddingTop: 27 * tmpWidth  }}>
+        <ScrollView 
+            contentContainerStyle={{backgroundColor: 'rgb(254,254,254)', paddingTop: 15 * tmpWidth, paddingBottom: 24 * tmpWidth}}
+            style={{backgroundColor: 'rgb(254,254,254)',}}
+        >
             <View style={styles.section}>
-                <Text style={{fontSize: 18 * tmpWidth , marginBottom: 14 * tmpWidth   }}>계정</Text>
+                <Text style={{fontSize: 16 * tmpWidth , marginBottom: 16 * tmpWidth   }}>계정</Text>
                 <TouchableOpacity>
                     <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)'}}>비밀번호 변경</Text>
                 </TouchableOpacity>
-                
             </View>
             <View style={styles.section}>
-                <Text style={{fontSize: 18 * tmpWidth , marginBottom: 14  * tmpWidth  }}>앱 설정</Text>
+                <Text style={{fontSize: 16 * tmpWidth , marginBottom: 16  * tmpWidth  }}>앱 설정</Text>
                 <TouchableOpacity>
                     <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)'}}>알림 설정</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.section}>
-                <Text style={{fontSize: 18 * tmpWidth , marginBottom: 14  * tmpWidth  }}>이용 안내</Text>
+                <Text style={{fontSize: 16 * tmpWidth , marginBottom: 14  * tmpWidth  }}>이용 안내</Text>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 12 * tmpWidth }}>공지사항</Text>
+                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 14 * tmpWidth }}>공지사항</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 12 * tmpWidth }}>서비스 이용약관</Text>
+                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 14 * tmpWidth }}>서비스 이용약관</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 12 * tmpWidth }}>개인정보 처리방침</Text>
+                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 14 * tmpWidth }}>개인정보 처리방침</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 12 * tmpWidth }}>오픈소스 라이선스</Text>
+                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)', marginBottom: 14 * tmpWidth }}>오픈소스 라이선스</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => navigate('FeedBack')}>
                     <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(118,118,118)' }}>피드백 및 건의사항</Text>
                 </TouchableOpacity>
             </View>
-            <View style={{marginLeft: 25 * tmpWidth   , paddingTop: 22 * tmpWidth   }}>
+            <View style={{marginLeft: 25 * tmpWidth, paddingTop: 24 * tmpWidth}}>
                 <TouchableOpacity>
-                <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(169,193,255)', marginBottom: 16 * tmpWidth   }}>회원 탈퇴</Text>
+                <Text style={{fontSize: 16 * tmpWidth, marginBottom: 22 * tmpWidth   }}>회원 탈퇴</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={async() => {
                     await NaverLogin.logout();
                     await deletenoticetoken();
                     await signout()}}>
-                    <Text style={{fontSize: 16 * tmpWidth , color: 'rgb(169,193,255)'}}>로그아웃</Text>
+                    <Text style={{fontSize: 16 * tmpWidth}}>로그아웃</Text>
                 </TouchableOpacity>
             </View>
-            <Text style={{marginLeft: 25 * tmpWidth, marginTop:tmpWidth*15,fontSize: 13 * tmpWidth , color: 'rgb(118,118,118)'}}>비밀번호 변경 및 계정 삭제는 </Text>
+            <Text style={{marginLeft: 25 * tmpWidth, marginTop:tmpWidth*24,fontSize: 13 * tmpWidth , color: 'rgb(118,118,118)'}}>비밀번호 변경 및 계정 삭제는 </Text>
             <Text style={{marginLeft: 25 * tmpWidth, marginTop:tmpWidth*5,fontSize: 13 * tmpWidth , color: 'rgb(118,118,118)'}}>Umpa.together@gmail.com로 문의 부탁드립니다.</Text>
         </ScrollView>
     )
@@ -93,8 +95,8 @@ const styles=StyleSheet.create({
         borderBottomWidth: 4  * tmpWidth ,
         borderBottomColor: 'rgba(196,196,196,0.2)',
         paddingLeft: 25  * tmpWidth ,
-        paddingBottom: 24  * tmpWidth ,
-        paddingTop: 22 * tmpWidth
+        paddingBottom: 18  * tmpWidth ,
+        paddingTop: 18 * tmpWidth
     }
 });
 
