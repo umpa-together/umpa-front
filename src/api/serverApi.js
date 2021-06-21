9a361179e4cf51ce6a15f0c6fa3218979ae7132c
blackSooooo <507pdophs@naver.com>
2021-06-22 01:09:11 +0900

    feat: add test branch

--- src/api/serverApi.js
+++ src/api/serverApi.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const instance =  axios.create({
-    baseURL: 'http://13.124.243.48'
-    //http://13.124.243.48
+const instance = axios.create({
+  baseURL: 'http://2a9351749e10.ngrok.io',
+  //http://13.124.243.48
 });
 
 instance.interceptors.request.use(
-    async(config)=>{
-        const token = await AsyncStorage.getItem('token');
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (err)=>{
-        return Promise.reject(err);
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
 );
 
 export default instance;
