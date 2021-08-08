91d427029caf29eef6b5d5a9bd0255c7762fd816
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-08 13:09:39 +0900

    fix: bottom tab, stack navigation refactor

--- src/navigator.js
+++ src/navigator.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext } from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useContext, useState } from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -9,6 +9,8 @@ import SvgUri from 'react-native-svg-uri';
 
 import { Context as AuthContext } from './context/AuthContext';
 import { Context as UserContext } from './context/UserContext';
+
+import LoadingPage from './screens/LoadingPage';
 
 import SigninScreen from './screens/SigninScreen';
 import SignupScreen from './screens/SignupScreen';
@@ -52,23 +54,98 @@ import SelectedSongScreen from './screens/Search/SelectedSongScreen';
 import SelectedHashtagScreen from './screens/Search/SelectedHashtagScreen';
 import AllContentsScreen from './screens/Search/AllContentsScreen';
 
-import { tmpWidth, tmpHeight } from './components/FontNormalize';
+import CreateModal from './components/CreateModal';
 
 const LoginStack = createNativeStackNavigator()
-const MainStack = createNativeStackNavigator()
-const SearchStack = createNativeStackNavigator()
-const BoardStack = createNativeStackNavigator()
-const NoticeStack = createNativeStackNavigator()
-const AccountStack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
+const MainStack = createNativeStackNavigator()
 
 const LoginStackScreen = () => {
     return (
-        <LoginStack.Navigator>
+        <LoginStack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             <LoginStack.Screen name="Signin" component={SigninScreen}/>
             <LoginStack.Screen name="Signup" component={SignupScreen}/>
             <LoginStack.Screen name="Signupopt" component={Signupopt}/>
         </LoginStack.Navigator>
+    )
+}
+const TabScreen = () => {
+    return (
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused }) => {
+                if (route.name === 'HomeTab') {
+                    return (
+                        <View>
+                            { focused ? 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedHome.svg')} /> : 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabHome.svg')} /> }
+                        </View>
+                    )
+                } else if (route.name === 'FeedTab') {
+                    return (
+                        <View>
+                            { focused ? 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedSearch.svg')} /> : 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabSearch.svg')} /> }
+                        </View>
+                    )
+                } else if (route.name === 'BoardTab') {
+                    return (
+                        <Text>
+                            +
+                        </Text>
+                    )
+                } else if (route.name === 'NoticeTab') {
+                    return (
+                        <View>
+                            { focused ? 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedNotice.svg')} /> : 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabNotice.svg')} /> }
+                        </View>
+                    )
+                } else if (route.name === 'AccountTab') {
+                    return ( 
+                        <View>
+                            { focused ? 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedAccount.svg')} /> : 
+                            <SvgUri width='40' height='40' source={require('./assets/icons/tabAccount.svg')} /> }
+                        </View>
+                    )
+                }
+            },
+            tabBarShowLabel: false,
+            tabBarStyle: {
+                backgroundColor: 'rgb(254,254,254)',
+                shadowColor: "rgb(0, 0, 0)",
+                shadowOffset: {
+                    height: 4,
+                    width: 0,
+                },
+                shadowRadius: 4,
+                shadowOpacity: 0.6,
+            },
+            headerShown: false,
+        })}>
+            <Tab.Screen name="HomeTab" component={MainSearchScreen}/>
+            <Tab.Screen name="FeedTab" component={MainPage}/>
+            <Tab.Screen 
+                name="BoardTab" 
+                component={MyModalBackgroundScreen}
+                listeners={({ navigation }) => ({
+                    tabPress: (e) => {
+                        e.preventDefault();
+                        navigation.navigate(`CreatePosts`)
+                    }
+                })}
+            />
+            <Tab.Screen name="NoticeTab" component={NoticeScreen}/>
+            <Tab.Screen name="AccountTab" component={AccountScreen}/>
+        </Tab.Navigator>
     )
 }
 
@@ -79,224 +156,64 @@ const MainStackScreen = () => {
                 headerShown: false
             }}
         >
-            <MainStack.Screen name="Main" component={MainPage} />
+            <MainStack.Screen name="Tab" component={TabScreen}/>
+            <MainStack.Screen name="Search" component={SearchScreen} />
+            <MainStack.Screen name="SearchResult" component={SearchResultScreen} />
+            <MainStack.Screen name="SelectedSong" component={SelectedSongScreen} />
             <MainStack.Screen name="Create" component={CreatePlayListPage} />
             <MainStack.Screen name="SearchSong" component={SearchSongPage} />
-            <MainStack.Screen name="CurationSearch" component={CurationSearchPage} />
-            <MainStack.Screen name="OtherAccount" component={OtherAccount} />
-            <MainStack.Screen name="Follow" component={FollowPage} />
+            <MainStack.Screen name="AllContents" component={AllContentsScreen} />
             <MainStack.Screen name="SelectedPlaylist" component={SelectedPlaylist} />
             <MainStack.Screen name="SelectedCuration" component={SelectedCuration} />
             <MainStack.Screen name="SelectedHashtag" component={SelectedHashtagScreen} />
+            <MainStack.Screen name="CurationSearch" component={CurationSearchPage} />
+            <MainStack.Screen name="SearchBoard" component={SearchBoardPage} />
+            <MainStack.Screen name="CreateBoard" component={CreateBoardPage} />
+            <MainStack.Screen name="SelectedBoard" component={SelectedBoardPage} />
+            <MainStack.Screen name="CreateContent" component={CreateContentPage} />
+            <MainStack.Screen name="SelectedContent" component={SelectedContentPage} />
+            <MainStack.Screen name="MyContents" component={MyContentsPage} />
+            <MainStack.Screen name="SearchContent" component={SearchContentPage} />
+            <MainStack.Screen name="MusicArchive" component={MusicArchivePage} />
+            <MainStack.Screen name="SearchMusic" component={SearchMusicPage} />
+            <MainStack.Screen name="MySharedSongs" component={MySharedSongsPage} />
+            <MainStack.Screen name="OtherAccount" component={OtherAccount} />
+            <MainStack.Screen name="Follow" component={FollowPage} />
+            <MainStack.Screen name="ProfileEdit" component={ProfileEditPage} />
+            <MainStack.Screen name="Setting" component={SettingPage} />
+            <MainStack.Screen name="SongEdit" component={SongEditPage} />
+            <MainStack.Screen name="FeedBack" component={FeedBackPage} />
+            <MainStack.Screen name="InformationUse" component={InformationUsePage} />
+            <MainStack.Screen name="MusicBox" component={MusicBoxScreen} />
+            <MainStack.Screen name="CreatePosts" component={CreateModal} 
+                options={{
+                    presentation: "transparentModal",
+                }}
+            />
         </MainStack.Navigator>
     )
 }
 
-const SearchStackScreen = () => {
-    return (
-        <SearchStack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <SearchStack.Screen name="MainSearch" component={MainSearchScreen} />
-            <SearchStack.Screen name="Search" component={SearchScreen} />
-            <SearchStack.Screen name="SearchResult" component={SearchResultScreen} />
-            <SearchStack.Screen name="SelectedSong" component={SelectedSongScreen} />
-            <SearchStack.Screen name="Create" component={CreatePlayListPage} />
-            <SearchStack.Screen name="SearchSong" component={SearchSongPage} />
-            <SearchStack.Screen name="AllContents" component={AllContentsScreen} />
-            <SearchStack.Screen name="OtherAccount" component={OtherAccount} />
-            <SearchStack.Screen name="Follow" component={FollowPage} />
-            <SearchStack.Screen name="SelectedPlaylist" component={SelectedPlaylist} />
-            <SearchStack.Screen name="SelectedCuration" component={SelectedCuration} />
-            <SearchStack.Screen name="SelectedHashtag" component={SelectedHashtagScreen} />
-        </SearchStack.Navigator>
-    )
-}
-
-const BoardStackScreen = () => {
-    return (
-        <BoardStack.Navigator
-            screenOptions={{
-                title: '게시판',
-                headerTitleStyle: {
-                    fontSize: 18 * tmpWidth,
-                    fontWeight: "400"
-                }, 
-                headerStyle: {
-                    backgroundColor: 'rgb(255,255,255)',
-                    height: 92 * tmpWidth,
-                    shadowColor: "rgb(0, 0, 0)",
-                    shadowOffset: {
-                        height: 3 * tmpWidth,
-                        width: 0,
-                    },
-                    shadowRadius: 8 * tmpWidth,
-                    shadowOpacity: 0.07,
-                },
-            }}
-        >
-            <BoardStack.Screen name="FreeBoard" component={FreeBoardPage} />
-            <BoardStack.Screen name="SearchBoard" component={SearchBoardPage} />
-            <BoardStack.Screen name="CreateBoard" component={CreateBoardPage} />
-            <BoardStack.Screen name="SelectedBoard" component={SelectedBoardPage} />
-            <BoardStack.Screen name="CreateContent" component={CreateContentPage} />
-            <BoardStack.Screen name="SelectedContent" component={SelectedContentPage} />
-            <BoardStack.Screen name="MyContents" component={MyContentsPage} />
-            <BoardStack.Screen name="SearchContent" component={SearchContentPage} />
-            <BoardStack.Screen name="MusicArchive" component={MusicArchivePage} />
-            <BoardStack.Screen name="SearchMusic" component={SearchMusicPage} />
-            <BoardStack.Screen name="MySharedSongs" component={MySharedSongsPage} />
-            <BoardStack.Screen name="OtherAccount" component={OtherAccount} />
-            <BoardStack.Screen name="Follow" component={FollowPage} />
-            <BoardStack.Screen name="SelectedPlaylist" component={SelectedPlaylist} />
-            <BoardStack.Screen name="SelectedCuration" component={SelectedCuration} />
-            <BoardStack.Screen name="SelectedHashtag" component={SelectedHashtagScreen} />
-        </BoardStack.Navigator>
-    ) 
-}
-
-const NoticeStackScreen = () => {
-    return (
-        <NoticeStack.Navigator
-            screenOptions={{
-                title: '알림',
-                headerTitleStyle: {
-                    fontSize: 18  * tmpWidth,
-                    fontWeight: "400",
-                }, 
-                headerStyle: {
-                    backgroundColor: 'rgb(255,255,255)',
-                    height: 92 * tmpHeight,
-                    shadowColor: "rgb(0, 0, 0)",
-                    shadowOffset: {
-                        height: 3  * tmpWidth,
-                        width: 0,
-                    },
-                    shadowRadius: 8  * tmpWidth,
-                    shadowOpacity: 0.07,
-                },            
-            }}
-        >
-            <NoticeStack.Screen name="Notice" component={NoticeScreen} />
-            <NoticeStack.Screen name="SelectedContent" component={SelectedContentPage} />
-            <NoticeStack.Screen name="MusicArchive" component={MusicArchivePage} />
-            <NoticeStack.Screen name="OtherAccount" component={OtherAccount} />
-            <NoticeStack.Screen name="Follow" component={FollowPage} />
-            <NoticeStack.Screen name="SelectedPlaylist" component={SelectedPlaylist} />
-            <NoticeStack.Screen name="SelectedCuration" component={SelectedCuration} />
-            <NoticeStack.Screen name="SelectedHashtag" component={SelectedHashtagScreen} />
-        </NoticeStack.Navigator>
-    ) 
-}
-
-const AccountStackScreen = () => {
-    return (
-        <AccountStack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-        >
-            <AccountStack.Screen name="Account" component={AccountScreen} />
-            <AccountStack.Screen name="OtherAccount" component={OtherAccount} />
-            <AccountStack.Screen name="Follow" component={FollowPage} />
-            <AccountStack.Screen name="ProfileEdit" component={ProfileEditPage} />
-            <AccountStack.Screen name="Setting" component={SettingPage} />
-            <AccountStack.Screen name="SongEdit" component={SongEditPage} />
-            <AccountStack.Screen name="SelectedPlaylist" component={SelectedPlaylist} />
-            <AccountStack.Screen name="SelectedCuration" component={SelectedCuration} />
-            <AccountStack.Screen name="SelectedHashtag" component={SelectedHashtagScreen} />
-            <AccountStack.Screen name="FeedBack" component={FeedBackPage} />
-            <AccountStack.Screen name="Create" component={CreatePlayListPage} />
-            <AccountStack.Screen name="SearchSong" component={SearchSongPage} />
-            <AccountStack.Screen name="InformationUse" component={InformationUsePage} />
-            <AccountStack.Screen name="MusicBox" component={MusicBoxScreen} />
-        </AccountStack.Navigator>
-    ) 
-}
-
-const MyModalBackgroundScreen = () => {
-    return null
-}
+const MyModalBackgroundScreen = () => null
 
 export default MainNavigator = () => {
     const { state: authState, tryLocalSignin } = useContext(AuthContext);
     const { getMyInfo } = useContext(UserContext);
-    
+    const [isSplash, setIsSplash] = useState(true)
+
     useEffect(() => {
         tryLocalSignin();
         getMyInfo();
     }, [])
 
+    if(isSplash){
+        return <LoadingPage setIsSplash={setIsSplash}/>
+    }
+
     return (
         <NavigationContainer ref={navigationRef}>
             {authState.token ? 
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused }) => {
-                        if (route.name === 'MainTab') {
-                            return (
-                                <View>
-                                    { focused ? 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedHome.svg')} /> : 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabHome.svg')} /> }
-                                </View>
-                            )
-                        } else if (route.name === 'SearchTab') {
-                            return (
-                                <View>
-                                    { focused ? 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedSearch.svg')} /> : 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabSearch.svg')} /> }
-                                </View>
-                            )
-                        } else if (route.name === 'BoardTab') {
-                            return (
-                                <View>
-                                    { focused ? 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedBoard.svg')} /> : 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabBoard.svg')} /> }
-                                </View>
-                            )
-                        } else if (route.name === 'NoticeTab') {
-                            return (
-                                <View>
-                                    { focused ? 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedNotice.svg')} /> : 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabNotice.svg')} /> }
-                                </View>
-                            )
-                        } else if (route.name === 'AccountTab') {
-                            return ( 
-                                <View>
-                                    { focused ? 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabFocusedAccount.svg')} /> : 
-                                    <SvgUri width='40' height='40' source={require('./assets/icons/tabAccount.svg')} /> }
-                                </View>
-                            )
-                        }
-                    },
-                    tabBarShowLabel: false,
-                    tabBarStyle: {
-                        backgroundColor: 'rgb(254,254,254)',
-                        shadowColor: "rgb(0, 0, 0)",
-                        shadowOffset: {
-                            height: 4,
-                            width: 0,
-                        },
-                        shadowRadius: 4,
-                        shadowOpacity: 0.6,
-                    },
-                    headerShown: false
-                })}
-            >
-                <Tab.Screen name="MainTab" component={MainStackScreen}/>
-                <Tab.Screen name="SearchTab" component={SearchStackScreen}/>
-                <Tab.Screen name="BoardTab" component={BoardStackScreen}/>
-                <Tab.Screen name="NoticeTab" component={NoticeStackScreen}/>
-                <Tab.Screen name="AccountTab" component={AccountStackScreen}/>
-            </Tab.Navigator> : 
+            <MainStackScreen /> : 
             <LoginStackScreen /> }
         </NavigationContainer>
     )
