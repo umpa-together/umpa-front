13940be352f29ab7c13799c193bd9caa53b98418
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-08 13:19:00 +0900

    feat: Add navHeader that has shadows, classify with isBack (boolean)

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,11 +16,37 @@ export default Header = ({ title }) => {
     )
 }
 
+export const NavHeader = ({ title, isBack = false }) => {
+    return (
+        <View style={styles.navContainer}>
+            <Text style={styles.title}>{title}</Text>
+            {isBack &&
+            <TouchableOpacity style={styles.back} onPress={goBack}>
+                <SvgUri width={40 * tmpWidth} height={40 * tmpWidth} source={require('../assets/icons/back.svg')}/>
+            </TouchableOpacity>}
+        </View>
+    )
+}
+
 const styles=StyleSheet.create({
     container: {
         width: '100%', 
-        height: 48 * tmpWidth, 
-        marginTop: StatusBarHeight * tmpWidth, 
+        height: (48 + StatusBarHeight) * tmpWidth, 
+        paddingTop: StatusBarHeight * tmpWidth,
+        backgroundColor: 'rgb(254,254,254)' 
+    },
+    navContainer: {
+        width: '100%',
+        height: (48 + StatusBarHeight) * tmpWidth, 
+        paddingTop: StatusBarHeight * tmpWidth,
+        backgroundColor: 'rgb(255, 255, 255)',
+        shadowColor: "rgb(0, 0, 0)",
+        shadowOffset: {
+            height: 3  * tmpWidth,
+            width: 0,
+        },
+        shadowRadius: 8  * tmpWidth,
+        shadowOpacity: 0.07,
     },
     title: {
         textAlign: 'center', 
@@ -30,6 +56,6 @@ const styles=StyleSheet.create({
     back: {
         position: 'absolute',
         left: 5 * tmpWidth, 
-        top: 2 * tmpWidth
+        top: (2 + StatusBarHeight) * tmpWidth
     }
 })
