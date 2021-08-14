8171c1d0e8fe0021a67c65e3ff66191094931027
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-14 18:04:02 +0900

    fix: conflicts resolve

--- src/api/serverApi.js
+++ src/api/serverApi.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 
 const instance = axios.create({
-  baseURL: 'http://13.124.243.48',
+  baseURL: 'http://56b9fd00b02c.ngrok.io',
   //http://13.124.243.48
 });
 
