afec3b26eb764e19e611926d6aa6ba7a588b21c1
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-14 20:04:40 +0900

    feat: Add MusicArchive components

---
+++ src/components/Main/MusicArchive.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { TouchableOpacity, FlatList, StyleSheet, Text } from 'react-native'
+import { SongImageBack } from '../SongImage'
+import { tmpWidth } from '../../components/FontNormalize'
+import SongStory from '../SongStory'
+
+export default MusicArchive = ({ archive }) => {
+    const [archiveModal, setArchiveModal] = useState(false)
+    const [story, setStory] = useState(null)
+    const onClickArchive = (songs) => {
+        setArchiveModal(true)
+        setStory(songs)
+    }
+
+    return (
+        <>
+            <FlatList 
+                data={archive}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={community => community._id._id}
+                renderItem={({item}) => {
+                    const { songs, _id: board } = item
+                    const backgroundImg = songs[0].attributes.artwork.url
+                    return (
+                        <TouchableOpacity 
+                            style={styles.archiveBox}
+                            onPress={() => onClickArchive(songs)}
+                        >
+                            <SongImageBack url={backgroundImg} width={120} height={150} border={8} opac={0.8} />
+                            <Text style={styles.name} numberOfLines={1}>{board.name}</Text>
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+            {archiveModal && <SongStory setArchiveModal={setArchiveModal} songs={story} />}
+        </>
+    )
+}
+
+const styles=StyleSheet.create({
+    archiveBox: {
+        width: 120 * tmpWidth,
+        height: 150 * tmpWidth,
+        borderRadius: 8 * tmpWidth,
+        marginRight: 8 * tmpWidth,
+    },
+    name: {
+        position: 'absolute',
+        bottom: 5 * tmpWidth,
+        left: 5 * tmpWidth
+    }
+})
