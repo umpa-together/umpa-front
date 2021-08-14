f0673496cdf6ed73deea73ac24e27a0a92b82b6e
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-14 20:04:28 +0900

    feat: Add SongStory components

---
+++ src/components/SongStory.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import Modal from 'react-native-modal';
+import { tmpWidth } from './FontNormalize';
+import { SongImageBack } from './SongImage'
+import { addtracksong, stoptracksong } from './TrackPlayer'
+import HarmfulModal from './HarmfulModal';
+
+export default SongStory = ({ songs, setArchiveModal }) => {
+    const [currentSong, setCurrentSong] = useState(songs[0])
+    const [index, setIndex] = useState(0)
+    const [isPlayingid, setIsPlayingid] = useState('0');
+    const [harmfulModal, setHarmfulModal] = useState(false);
+
+    const onClose = () => {
+        setArchiveModal(false)
+        stoptracksong({ setIsPlayingid })
+    }
+
+    const onClickPrevious = () => {
+        if(index === 0) return
+        setCurrentSong(songs[index-1])
+        setIndex((prev) => prev-1)
+        addtracksong({ data: songs[index-1], setIsPlayingid, setHarmfulModal })
+    }
+
+    const onClickNext = () => {
+        if(index === songs.length - 1)  return
+        setCurrentSong(songs[index+1])
+        setIndex((prev) => prev+1)
+        addtracksong({ data: songs[index+1], setIsPlayingid, setHarmfulModal })
+    }
+
+    useEffect(() => {
+        addtracksong({ data: currentSong, setIsPlayingid, setHarmfulModal })
+    }, [])
+
+    return (
+        <Modal
+            animationIn="fadeIn"
+            animationOut="fadeOut"
+            isVisible={true}
+            onBackdropPress={onClose}
+            backdropOpacity={0.4}
+            style={{margin: 0, zIndex: 97}}
+        >
+            <TouchableOpacity onPress={onClose} style={{zIndex: 98}}>
+                <Text style={styles.exit}>나가기</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onClickPrevious} style={{zIndex: 98}}>
+                <Text style={styles.before}>이전</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={onClickNext} style={{zIndex: 98}}>
+                <Text style={styles.next}>다음</Text>
+            </TouchableOpacity>
+            <SongImageBack url={currentSong.attributes.artwork.url} width={375} height={812} opac={1} border={0} />
+            { harmfulModal && <HarmfulModal harmfulModal={harmfulModal} setHarmfulModal={setHarmfulModal}/> }
+        </Modal>
+    )
+}
+
+const styles=StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 875,
+        backgroundColor: 'black'
+    },
+    exit: {
+        position: 'absolute',
+        top: 100,
+        color: 'white',
+    },
+    before: {
+        position: 'absolute',
+        top: 100,
+        left: 150,
+        color: 'white',
+    }, 
+    next: {
+        position: 'absolute',
+        top: 100,
+        color: 'white',
+        left: 300,
+    }
+})
