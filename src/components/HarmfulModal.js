c0bb57e431cfee184e83148460ec6f0a06d53b0d
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-16 15:47:11 +0900

    fix: harmfulModal refactor with modal providers

--- src/components/HarmfulModal.js
+++ src/components/HarmfulModal.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Modal from 'react-native-modal';
 import { tmpWidth } from './FontNormalize';
+import { useModal } from '../providers/modal';
 
-const HarmfulModal = ({ harmfulModal, setHarmfulModal }) => {
-    const onClose = () =>{
-        setHarmfulModal(false);
-    };
+const HarmfulModal = () => {
+    const { harmfulModal, onCloseModal } = useModal()
+
     return (
         <Modal
             animationIn="fadeIn"
             animationOut="fadeOut"
             isVisible={harmfulModal}
             backdropOpacity={0.4}
-            onBackdropPress={onClose}
+            onBackdropPress={onCloseModal}
             style={{margin: 0, alignItems: 'center'}}
         >
             <View style={styles.harmfulContainer}>
                 <Text style={{fontSize: 16 * tmpWidth, color: 'rgb(86,86,86)', marginTop: 24 * tmpWidth}}>유해성 컨텐츠는 현재 감상할 수 없습니다.</Text>
-                <TouchableOpacity onPress={() => onClose()}>
+                <TouchableOpacity onPress={onCloseModal}>
                     <Text style={{fontSize: 16 * tmpWidth, marginTop: 16 * tmpWidth}}>확인</Text>
                 </TouchableOpacity>
             </View>
