60e1f8154177e5fc1e7c2592cbaa21e8ffd43144
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-08-18 21:39:14 +0900

    fix: err fix in lists.slice

--- src/components/Main/SearchResult.js
+++ src/components/Main/SearchResult.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native'
 import { Context as SearchPlaylistContext } from 'context/SearchPlaylistContext';
+import LoadingIndicator from '../LoadingIndicator'
 
 export default SearchResult = () => {
     const { state } = useContext(SearchPlaylistContext);
@@ -10,41 +11,45 @@ export default SearchResult = () => {
         lists: state.dj
     }, {
         title: '플레이리스트',
-        lists: state.playlists
-    }, {
+        lists: state.playList
+    }, 
+    /*{
         title: '데일리',
         lists: state.daily
-    }]
+    }*/]
 
     return (
         <>
-            <Text>전체보기</Text>
-            {optionLists.map(({ title, lists }) => {
-                return (
-                    <>
-                        <View style={styles.header}>
-                            <Text>{title}</Text>
-                            <TouchableOpacity>
-                                <Text>{'>'}</Text>
-                            </TouchableOpacity>
-                        </View>
-                        <FlatList 
-                            horizontal
-                            showsHorizontalScrollIndicator={false}                            
-                            data={lists.slice(0, 10)}
-                            keyExtractor={item=>item._id}
-                            renderItem={({item}) => {
-                                const image = item.image || item.profileImage
-                                return (
-                                    <TouchableOpacity>
-                                        <Image source={{uri: image}} style={{width: 150 ,height: 150, marginRight: 20}} />
-                                    </TouchableOpacity>
-                                )
-                            }}
-                        />
-                    </>
-                )
-            })}
+            {state.dj === null && state.playList === null ? <LoadingIndicator /> :
+            <>
+                <Text>전체보기</Text>
+                {optionLists.map(({ title, lists }) => {
+                    return (
+                        <>
+                            <View style={styles.header}>
+                                <Text>{title}</Text>
+                                <TouchableOpacity>
+                                    <Text>{'>'}</Text>
+                                </TouchableOpacity>
+                            </View>
+                            <FlatList 
+                                horizontal
+                                showsHorizontalScrollIndicator={false}                            
+                                data={lists.slice(0, 10)}
+                                keyExtractor={item=>item._id}
+                                renderItem={({item}) => {
+                                    const image = item.image || item.profileImage
+                                    return (
+                                        <TouchableOpacity>
+                                            <Image source={{uri: image}} style={{width: 150 ,height: 150, marginRight: 20}} />
+                                        </TouchableOpacity>
+                                    )
+                                }}
+                            />
+                        </>
+                    )
+                })}
+            </> }
         </>
     )
 }
