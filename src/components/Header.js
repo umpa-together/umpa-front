e8cba4633a43f4b65569216867b21d76c1ff4bb3
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-10 14:02:49 +0900

    fix: add temporary Playlist Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { tmpWidth } from 'components/FontNormalize'
 import { StatusBarHeight } from 'components/StatusBarHeight'
 import SvgUri from 'react-native-svg-uri'
 import { goBack } from 'navigationRef'
+import TextTicker from 'react-native-text-ticker'
 
 export default Header = ({ title }) => {
     return (
@@ -28,12 +29,32 @@ export const NavHeader = ({ title, isBack = false }) => {
     )
 }
 
+export const PlaylistHeader = ({ title }) => {
+    return (
+        <View style={styles.container}>
+            <TextTicker
+                duration={5000}
+                bounce={false}
+                marqueeDelay={1000}
+                style={styles.title}
+            >
+                {title}
+            </TextTicker>
+            <TouchableOpacity style={styles.back} onPress={goBack}>
+                <SvgUri width={40 * tmpWidth} height={40 * tmpWidth} source={require('assets/icons/back.svg')}/>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
 const styles=StyleSheet.create({
     container: {
         width: '100%', 
         height: (48 + StatusBarHeight) * tmpWidth, 
         paddingTop: StatusBarHeight * tmpWidth,
-        backgroundColor: 'rgb(254,254,254)' 
+        backgroundColor: 'rgb(254,254,254)',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     navContainer: {
         width: '100%',
@@ -51,7 +72,8 @@ const styles=StyleSheet.create({
     title: {
         textAlign: 'center', 
         fontSize: 18 * tmpWidth, 
-        paddingTop: 18 * tmpWidth
+        //width: 200 * tmpWidth, 
+        borderWidth: 1
     },
     back: {
         position: 'absolute',
