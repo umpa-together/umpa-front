5fdcc3b5bff54895f7f66f551aa9b5ebeed2310c
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-12 19:02:12 +0900

    fix: add playTime, set to currentSong when click start songs with musicBar

--- src/providers/trackPlayer/index.js
+++ src/providers/trackPlayer/index.js
@@ -8,6 +8,8 @@ export const useTrackPlayer = () => useContext(TrackPlayerContext)
 
 export default TrackPlayerProvider = ({ children }) => {
     const [isPlayingId, setIsPlayingId] = useState('0')
+    const [currentSong, setCurrentSong] = useState(null)
+    const [playTime, setPlayTime] = useState(0)
     const { setHarmfulModal } = useModal()
 
     const addtracksong= async ({ data }) => {
@@ -24,24 +26,35 @@ export default TrackPlayerProvider = ({ children }) => {
         } else {
             setHarmfulModal(true);
         }
+        setCurrentSong(data)
     };
     
     const stoptracksong= () => {    
         setIsPlayingId('0');
+        setCurrentSong(null)
         TrackPlayer.reset()
     };
 
     const value = {
         isPlayingId,
+        currentSong,
+        playTime,
         addtracksong,
         stoptracksong,
     }
 
     useEffect(() => {
-        const trackPlayer = setTimeout(() => setIsPlayingId('0'), 30000);
+        const trackPlayer = setTimeout(() => setIsPlayingId('0'), 31000);
+        setPlayTime(0)
         return () => clearTimeout(trackPlayer);
     }, [isPlayingId])
 
+    useEffect(() => {
+        const timeInterval = setInterval(() => setPlayTime((prev) => prev+1), 1000)
+        return () => {
+            if(playTime === 30) clearInterval(timeInterval)
+        }
+    }, [])
     return (
         <TrackPlayerContext.Provider value={value}>{children}</TrackPlayerContext.Provider>
     )
