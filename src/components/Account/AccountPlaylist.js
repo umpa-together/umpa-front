9d422d70d1be63e3ae7f3ce65382168fb6b4d49e
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-14 00:21:06 +0900

    fix: layout change account-playlists

--- src/components/Account/AccountPlaylist.js
+++ src/components/Account/AccountPlaylist.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { tmpWidth } from 'components/FontNormalize';
 import { Context as PlaylistContext } from 'context/PlaylistContext';
-import { push } from 'navigationRef';
-const AccountPlaylist = ({ playList }) => {
+import { navigate, push } from 'navigationRef';
+
+const AccountPlaylist = ({ playList, isMyAccount }) => {
     playList.sort(function(a,b){
         if(a.time > b.time)  return -1;
         if(a.time  < b.time) return 1;
@@ -12,60 +13,96 @@ const AccountPlaylist = ({ playList }) => {
     });
     const { getPlaylist } = useContext(PlaylistContext);
 
+    const onClickPlaylist = async (id, postUser) => {
+        await getPlaylist({ id, postUserId: postUser })
+        push('SelectedPlaylist', { id, postUser })
+    }
+
+    const onClickMakePlaylist = () => {
+        navigate('Create', {'data': []})
+    }
+
     return (
-        <View style={{flexDirection: 'row', flexWrap: 'wrap', paddingLeft: 20 * tmpWidth}}>
-            {playList.map((item) => {
-                return (
+        <>
+            {playList.length !== 0 ?
+            <View style={styles.container}>
+                {playList.map(({ image, _id: id, postUserId: postUser }) => {
+                    return (
+                        <TouchableOpacity 
+                            onPress={() => onClickPlaylist(id, postUser)}
+                            key={id}
+                        >
+                            <Image 
+                                style={styles.playlist} 
+                                source={{ url: image }} 
+                            />
+                        </TouchableOpacity>
+                    )
+                })}
+            </View> : 
+            <View style={styles.makeContainer}>
+                <Text style={styles.title}>아직 만들어진 플레이리스트가 없어요.</Text>
+                { isMyAccount && 
+                <View style={styles.myBox}>
+                    <Text style={styles.subTitle}>나만의 플레이리스트를 만들어보세요.</Text>   
                     <TouchableOpacity 
-                        style={styles.postBox}
-                        onPress={async ()=>{ 
-                            await getPlaylist({id:item._id, postUserId:item.postUserId})
-                            push('SelectedPlaylist', {id: item._id , postUser: item.postUserId})
-                        }}
-                        key={item._id}
+                        style={styles.makeBox}
+                        onPress={onClickMakePlaylist}
                     >
-                        <View style={{alignItems: 'center'}}>
-                            <View style={styles.thumbnail}>
-                                <Image  style={{height:'100%', width:'100%', borderRadius: 8 * tmpWidth}} source={{url:item.image}} />
-                            </View>
-                            <View style={styles.titleBox}>
-                                <Text numberOfLines={2} style={{fontSize: 14 * tmpWidth, color: 'rgb(79,79,79)'}}>{item.title}</Text>
-                            </View>
-                            <View style={styles.hashtagBox}>
-                                { item.hashtag != undefined ? 
-                                <Text numberOfLines={1} style={{fontSize: 12 * tmpWidth, color:'rgb(153,153,153)', paddingRight: 6 * tmpWidth}}>{item.hashtag.map(item => {
-                                    return '#'+item+'   '
-                                })}</Text> : null }
-                            </View>                       
-                        </View>
+                        <Text style={styles.boxText}>플레이리스트 만들기</Text>
                     </TouchableOpacity>
-                )
-           })}
-         </View>
+                </View> }
+            </View> }
+        </>
 
     );
 };
 
 const styles=StyleSheet.create({
-    postBox: {
-        height:223*tmpWidth, 
-        width:161*tmpWidth, 
-        marginTop: 14 * tmpWidth,
-        marginRight: 12 * tmpWidth
+    container: {
+        flexDirection: 'row', 
+        flexWrap: 'wrap', 
+        paddingLeft: 8 * tmpWidth,
+        paddingTop: 8 * tmpWidth,
     },
-    thumbnail: {
-        width: 161 * tmpWidth ,
-        height: 157 * tmpWidth,
-        borderRadius: 8 * tmpWidth
+    makeContainer: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 78 * tmpWidth
     },
-    titleBox: {
-        width: 161 * tmpWidth ,
-        marginTop: 10 * tmpWidth,
+    playlist: {
+        width: 117 * tmpWidth,
+        height: 117 * tmpWidth,
+        borderRadius: 4 * tmpWidth,
+        marginRight: 4 * tmpWidth,
+        marginBottom: 4 * tmpWidth
     },
-    hashtagBox: {
-        width: 161 * tmpWidth,
-        marginTop: 8  * tmpWidth,
-        flexDirection: 'row',
+    title: {
+        fontSize: 16 * tmpWidth,
+        fontWeight: '400',
+        marginBottom: 4 * tmpWidth
+    },
+    subTitle: { 
+        fontSize: 14 * tmpWidth,
+        fontWeight: '400',
+        color: '#8c8c8c',
+        marginBottom: 16 * tmpWidth
+    },
+    myBox: {
+        alignItems: 'center'
+    },
+    makeBox: {
+        width: 156 * tmpWidth,
+        height: 33 * tmpWidth,
+        borderRadius: 100 * tmpWidth,
+        borderColor: '#8bc0ff',
+        borderWidth: 1 * tmpWidth,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    boxText: {
+        fontSize: 14 * tmpWidth,
+        color: '#8bc0ff'
     }
 });
 
