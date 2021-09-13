ae16d3a85b115ed8fe72224d49060b73d2b8c49e
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-14 00:26:39 +0900

    refactor: refactor in Account

--- src/screens/Account/OtherAccount.js
+++ src/screens/Account/OtherAccount.js
@@ -1,14 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import { Context as UserContext } from 'context/UserContext';
 import AccountPlaylist from  'components/Account/AccountPlaylist';
 import AccountCurating from  'components/Account/AccountCurating';
 import Header from 'components/Account/Header'
-import SongProfile from 'components/Account/SongProfile'
-import FollowBox from 'components/Account/FollowBox';
-import Introduction from 'components/Account/Introduction'
 import Menu from 'components/Account/Menu'
 import LoadingIndicator from 'components/LoadingIndicator'
+import ProfileSong from 'components/Account/ProfileSong'
+import Profile from 'components/Account/Profile'
+import { tmpWidth } from 'components/FontNormalize'
+import ProfileButton from 'components/Account/Button'
+import Information from 'components/Account/Information'
 
 require('date-utils');
 
@@ -20,7 +22,8 @@ const OtherAccountScreen = ({ route }) => {
     const [url, setUrl] = useState('');
     const [user, setUser] = useState(null);   
     const { otherUserId: id } = route.params
-    
+    const [FollwerNum, setFollowerNum] = useState(0)
+
     useEffect(() => {
         const trackPlayer = setTimeout(() => setIsPlayingid('0'), 30000);
         return () => clearTimeout(trackPlayer);
@@ -31,6 +34,7 @@ const OtherAccountScreen = ({ route }) => {
             var newDate = new Date();
             let time = newDate.toFormat('YYYY-MM-DD');
             if(userState.otherUser.todaySong != undefined)  setStory(userState.otherUser.todaySong.filter(item => item.time == time)[0]);
+            setFollowerNum(user.follower.length)
         }
     }, [user]);
 
@@ -47,23 +51,28 @@ const OtherAccountScreen = ({ route }) => {
     }, [id]);
 
     return (
-        <View style={{flex:1,backgroundColor: 'rgb(250,250,250)'}}>
+        <View style={styles.container}>
             {user === null ? <LoadingIndicator /> :
-            <View style={{flex: 1}}>
+            <View style={styles.flex}>
                 <Header user={user} isMyAccount={false} />
                 <ScrollView 
                     showsVerticalScrollIndicator={false}
                     stickyHeaderIndices={[1]}
                 >
                     <View>
-                        <SongProfile user={user} url={url} story={story} isMyAccount={false} />
-                        <FollowBox user={user} isMyAccount={false} />
-                        <Introduction user={user} />
+                        <View style={styles.profile}>
+                            <View style={styles.column}>
+                                <Profile user={user} isMyAccount={false} url={url} story={story} />
+                                <ProfileButton isMyAccount={false} user={user} setFollowerNum={setFollowerNum} />
+                            </View>
+                            <Information user={user} followerNum={FollwerNum}/>
+                        </View>
+                        <ProfileSong song={user.songs} isMyAccount={false} />
                     </View>
                     <View>
-                        <Menu user={user} menu={menu} setMenu={setMenu} />
+                        <Menu menu={menu} setMenu={setMenu} />
                     </View>
-                    <View style={{backgroundColor: 'rgb(255,255,255)'}}>
+                    <View style={styles.background}>
                         {menu == 'playlist' ?  <AccountPlaylist playList={user.playlists} /> :
                         <AccountCurating curating={user.curationposts} />}
                     </View>
@@ -72,5 +81,26 @@ const OtherAccountScreen = ({ route }) => {
         </View>
     )
 };
+
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff'
+    },
+    flex: {
+        flex: 1
+    },
+    profile: {
+        flexDirection: 'row', 
+        marginLeft: 22 * tmpWidth
+    },
+    background: {
+        backgroundColor: '#ffffff'
+    },
+    column: {
+        flexDirection: 'column', 
+        alignItems: 'center'
+    }
+})
 
 export default OtherAccountScreen;
