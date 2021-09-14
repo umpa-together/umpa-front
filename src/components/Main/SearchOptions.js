d23ab2f75390da261f4854e334e07b3544309318
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-15 00:12:18 +0900

    fix: Add initPlaylist, hint state to true when click another options

--- src/components/Main/SearchOptions.js
+++ src/components/Main/SearchOptions.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Context as SearchPlaylistContext } from 'context/SearchPlaylistContext';
 import { useSearch } from '../../providers/search';
+import { tmpWidth } from 'components/FontNormalize';
 
 export default SearchOption = () => {
-    const { searchOption, setSearchOption, textRef, setText } = useSearch()
-    const optionLists = ['곡, 아티스트', '계정', '해시태그']
-    const searchLists = ['Song', 'DJ', 'Hashtag']
+    const { initPlaylist } = useContext(SearchPlaylistContext)
+    const { searchOption, setSearchOption, textRef, setText, setIsHint } = useSearch()
+    const optionLists = ['계정', '곡, 아티스트', '해시태그']
+    const searchLists = ['DJ', 'Song', 'Hashtag']
 
     const onClickOption = (index) => {
         setSearchOption(searchLists[index])
         textRef.current.clear()
         setText('')
+        setIsHint(true)
+        initPlaylist()
     }
 
     return (
@@ -18,9 +23,9 @@ export default SearchOption = () => {
             {optionLists.map((item, index) => {
                 return (
                     <TouchableOpacity
-                        key={item}
+                        key={Math.random()}
                         onPress={() => onClickOption(index)}
-                        style={searchLists[index] === searchOption ? styles.activeBox : styles.box}
+                        style={searchLists[index] === searchOption ? [styles.box, styles.active] : styles.box}
                     >
                         <Text style={searchLists[index] === searchOption ? styles.activeText : styles.text}>{item}</Text>
                     </TouchableOpacity>
@@ -33,29 +38,25 @@ export default SearchOption = () => {
 const styles=StyleSheet.create({
     flexRow: {
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingLeft: 18 * tmpWidth,
+        marginTop: 16 * tmpWidth
     },
     box: {
-        paddingLeft: 12,
-        paddingRight: 12,
-        paddingTop: 8,
-        paddingBottom: 8,
-        borderWidth: 1,
-        borderRadius: 20,
-        borderColor: '#a9c1ff'
+        paddingLeft: 19 * tmpWidth,
+        paddingRight: 19 * tmpWidth,
+        paddingTop: 7 * tmpWidth,
+        paddingBottom: 7 * tmpWidth,
+        borderRadius: 100 * tmpWidth,
+        borderColor: '#8bc0ff',
+        borderWidth: 1 * tmpWidth,
+        marginRight: 10 * tmpWidth
     },
-    activeBox: {
-        paddingLeft: 12,
-        paddingRight: 12,
-        paddingTop: 8,
-        paddingBottom: 8,
-        borderWidth: 1,
-        borderRadius: 20,
-        backgroundColor: '#a9c1ff',
-        borderColor: '#a9c1ff'
+    active: {
+        backgroundColor: '#8bc0ff',
     },
     text: {
-        color: '#a9c1ff'
+        color: '#8bc0ff'
     },
     activeText: {
         color: 'white'
