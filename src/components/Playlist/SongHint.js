7a246892dd7e110f0e862a57fa6b13d98efeec78
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-16 12:55:19 +0900

    feat: Add new components related with SearchView(MyArchiveLists, SearchView, SongHint, SongResult)

---
+++ src/components/Playlist/SongHint.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react'
+import { Text, View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { Context as SearchContext } from 'context/SearchContext';
+import { useSearch } from '../../providers/search';
+import { tmpWidth } from 'components/FontNormalize';
+
+export default SongHint = () => {
+    const { state: searchState, searchsong } = useContext(SearchContext);
+    const { setIsHint } = useSearch()
+
+    const onClickHint = (hint) => {
+        searchsong({ songname: hint })
+        setIsHint(false)
+    }
+
+    return (
+        <FlatList 
+            keyboardShouldPersistTaps="handled"
+            data={searchState.hint}
+            keyExtractor={term => term}
+            renderItem={({item}) => {
+                return (
+                    <TouchableOpacity
+                        onPress={() => onClickHint(item)}
+                        style={styles.box}
+                    >
+                        <View style={styles.hintArea}>
+                            <Text style={styles.hint} numberOfLines={1}>{item}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )
+            }}
+        
+        />
+    )
+}
+
+const styles=StyleSheet.create({
+    box: {
+        height: 37 * tmpWidth, 
+        paddingLeft: 18 * tmpWidth, 
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    hintArea: {
+        width: 330 * tmpWidth,
+    },
+    hint: {
+        fontSize: 16 * tmpWidth,
+        fontWeight: '400'
+    }
+})
