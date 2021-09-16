5af28b245ad6bcfe4cd6d5bfbc60faa96dc0b8f5
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-16 13:00:04 +0900

    fix: add report, edit, delete button

--- src/components/Playlist/PlaylistsLikes.js
+++ src/components/Playlist/PlaylistsLikes.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Context as PlaylistContext } from 'context/PlaylistContext';
 import { Context as UserContext } from 'context/UserContext';
@@ -6,12 +6,19 @@ import { tmpWidth } from 'components/FontNormalize';
 import ProfileImage from 'components/ProfileImage'
 import SvgUri from 'react-native-svg-uri';
 import { usePlaylist } from 'providers/playlist';
+import DeleteModal from 'components/DeleteModal';
+import Modal from 'react-native-modal';
+import ReportModal from 'components/ReportModal';
+import { navigate } from 'navigationRef';
 
 export default PlaylistsLikes = ({ playlist }) => {
     const { likesPlaylist, unlikesPlaylist } = useContext(PlaylistContext);
     const { state: userState } = useContext(UserContext);
-    const { postUserId: postUser, likes } = playlist
+    const { postUserId: postUser, likes, songs } = playlist
     const { onClickProfile } = usePlaylist()
+    const [deleteModal, setDeleteModal] = useState(false);
+    const [weeklyModal, setWeeklyModal] = useState(false);
+    const [reportModal, setReportModal] = useState(false);
 
     const onClickLikes = () => {
         if(likes.includes(userState.myInfo._id)) {
@@ -19,6 +26,22 @@ export default PlaylistsLikes = ({ playlist }) => {
         } else {
             likesPlaylist({ id: playlist._id })
         }
+    }
+
+    const onClickEdit = () => {
+        navigate('Create', {'data': songs, 'isEdit': true})
+    }
+    const onClickDelete = () => {
+        if(!playlist.isWeekly){
+            setDeleteModal(true)
+        }else {
+            setWeeklyModal(true)
+            setTimeout(() => setWeeklyModal(false), 1200);
+        }
+    }
+
+    const onClickReport = () => {
+        setReportModal(true)
     }
 
     return (
@@ -31,12 +54,45 @@ export default PlaylistsLikes = ({ playlist }) => {
                 </TouchableOpacity>
                 <Text style={styles.name}>{postUser.name}</Text>
             </View>
-            <TouchableOpacity onPress={onClickLikes}>
-                <SvgUri 
-                    width={40} height={40} 
-                    source={likes.includes(userState.myInfo._id) ? require('assets/icons/playlistHearto.svg') : require('assets/icons/playlistHeart.svg')}
-                /> 
-            </TouchableOpacity>
+            <View style={styles.flexRow}>
+                { userState.myInfo._id === postUser._id ?
+                <View style={styles.flexRow}>
+                    <TouchableOpacity
+                        onPress={onClickEdit}
+                    >
+                        <Text style={styles.text}>수정</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={onClickDelete}
+                    >
+                        <Text style={styles.text}>삭제</Text>
+                    </TouchableOpacity>
+                </View> : 
+                <TouchableOpacity
+                    onPress={onClickReport}
+                >
+                    <Text style={styles.text}>신고</Text>
+                </TouchableOpacity> }
+                <TouchableOpacity onPress={onClickLikes}>
+                    <SvgUri 
+                        width={40} height={40} 
+                        source={likes.includes(userState.myInfo._id) ? require('assets/icons/playlistHearto.svg') : require('assets/icons/playlistHeart.svg')}
+                    /> 
+                </TouchableOpacity>
+            </View>
+            { deleteModal && <DeleteModal deleteModal={deleteModal} setDeleteModal={setDeleteModal} type={'playlist'} /> }
+            { reportModal && <ReportModal reportModal={reportModal} setReportModal={setReportModal} type={'playlist'} subjectId={playlist._id} /> }
+            <Modal
+                animationIn="fadeIn"
+                animationOut="fadeOut"
+                isVisible={weeklyModal}
+                backdropOpacity={0.5}
+                style={{alignItems: 'center'}}
+            >
+                <View style={styles.modalBox}>
+                    <Text style={styles.modalText}>위클리 플레이리스트는 삭제 불가능합니다.</Text>
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -61,5 +117,22 @@ const styles=StyleSheet.create({
     name: {
         fontSize: 14 * tmpWidth,
         marginLeft: 11 * tmpWidth
+    },
+    text: {
+        fontSize: 13 * tmpWidth,
+        fontWeight: '400',
+        marginRight: 8 * tmpWidth
+    },
+    modalBox: {
+        backgroundColor: '#ffffff', 
+        paddingTop: 20 * tmpWidth, 
+        paddingBottom: 20 *tmpWidth, 
+        paddingLeft: 10 * tmpWidth, 
+        paddingRight: 10 * tmpWidth, 
+        borderRadius: 8 * tmpWidth
+    },
+    modalText: {
+        fontSize: 14 * tmpWidth, 
+        color: 'rgb(86,86,86)', 
     }
 })
