9335ae9c238f91bca3941cc6c19c4dbb5bd357ac
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-16 15:44:21 +0900

    fix: change layout, code refactor in profile edit

--- src/components/Account/ProfileEdit.js
+++ src/components/Account/ProfileEdit.js
@@ -1,75 +1,45 @@
-import React, { useState, useContext } from 'react'
-import { Text, View, StyleSheet, TextInput, Keyboard } from 'react-native';
-import { Context as AuthContext } from 'context/AuthContext';
+import React from 'react'
+import { View, StyleSheet, Keyboard } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import SvgUri from 'react-native-svg-uri';
 import { tmpWidth } from 'components/FontNormalize';
+import EditFields from './EditFields'
 
-export default ProfileEdit = ({ nameValidity, doubleCheck, information }) => {
-    const { state: authState } = useContext(AuthContext);
-    const [name, setName] = useState(information.userName)
-    const [introduction, setIntroduction] = useState(information.introduction)
+export default ProfileEdit = ({ nameValidity, information }) => {
 
-    const onChangeName = (text) => {
-        if(text.length <= 15) information.userName = text
-        setName(information.userName)
-    }
-
-    const onChangeIntroduction = (text) => {
-        information.introduction = text
-        setIntroduction(information.introduction)
-    }
+    const fieldLists = [{
+        title: '닉네임',
+        validity: nameValidity,
+    }, {
+        title: '이름',
+        validity: true,
+    }, {
+        title: '소개글',
+        validity: true,
+    }]
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View>
-                <View style={styles.nameBox}>
-                    <Text style={styles.title}>이름</Text>
-                    <View style={styles.flex}>
-                        <View style={nameValidity ? styles.input : styles.validityInput}>
-                            <TextInput
-                                value={name}
-                                onChangeText={(text) => onChangeName(text)}
-                                placeholder="사용자 이름을 적어주세요. (15자 이내)"
-                                placeholderTextColor='rgb(196,196,196)'
-                                autoCapitalize='none'
-                                autoCorrect={false}
-                                style={styles.textArea}
-                            />
-                        </View>
-                        { doubleCheck && !authState.doubleCheck && 
-                        <View style={styles.warningBox}>
-                            <SvgUri width='14' height='14' source={require('assets/icons/warning.svg')}/>
-                            <Text style={styles.warningText}>현재 사용중인 아이디입니다.</Text>
-                        </View> }
-                        { !nameValidity  &&
-                        <View style={styles.warningBox}>
-                            <SvgUri width='14' height='14' source={require('assets/icons/warning.svg')}/>
-                            <Text style={styles.warningText}>사용자 이름을 입력해주세요.</Text>
-                        </View> }
-                    </View>
-                </View>
-                <View style={styles.introduceContainer}>
-                    <Text style={styles.title}>소개</Text>
-                    <View style={styles.introduceBox}>
-                        <TextInput
-                            value={introduction}
-                            onChangeText={(text) =>  onChangeIntroduction(text)}
-                            placeholder="소개글을 적어주세요."
-                            placeholderTextColor='rgb(196,196,196)'
-                            autoCapitalize='none'
-                            autoCorrect={false}
-                            multiline={true}
-                            style={styles.introducetextinput}
+            <View style={styles.container}>
+                {fieldLists.map(({ title, validity }) => {
+                    return (
+                        <EditFields 
+                            title={title} 
+                            information={information} 
+                            validity={validity}
+                            key={title}
                         />
-                    </View>
-                </View>
+                    )
+                })}
             </View>
         </TouchableWithoutFeedback>
     )
 }
 
 const styles=StyleSheet.create({
+    container: {
+        marginLeft: 18 * tmpWidth,
+        marginRight: 18 * tmpWidth
+    },
     nameBox: {
         width: 327 * tmpWidth,
         height: 44 * tmpWidth,
