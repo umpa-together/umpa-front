b448a7fd93485f80dab016360ecbcfbd64cb95fb
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-20 20:36:43 +0900

    fix: code refactor in feed and story

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
-import { View, SafeAreaView } from 'react-native';
-import {Context as PlaylistContext} from '../../context/PlaylistContext';
-import {Context as UserContext} from '../../context/UserContext';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context as PlaylistContext } from 'context/PlaylistContext';
+import { Context as UserContext } from 'context/UserContext';
+import { StatusBarHeight } from 'components/StatusBarHeight'
+import { tmpWidth } from 'components/FontNormalize'
 import Playlist from  './Playlist';
 import Story from './Story';
-import StoryProvider from '../../providers/story';
-import StoryModal from './StoryModal';
+import StoryProvider from 'providers/story';
+import SelectedStory from './SelectedStory';
 
 const Feed = () => {
     const { state } = useContext(PlaylistContext);
@@ -13,16 +15,31 @@ const Feed = () => {
 
     return (
         <StoryProvider>
-            <SafeAreaView style={{backgroundColor:"rgb(254,254,254)", flex: 1}}>
-                <View style={{flex:1}}>
-                    <Story story={userState.otherStory} />
-                    <Playlist playList={state.playlists} />
-                </View>          
-            </SafeAreaView>
-            <StoryModal />
+            <View style={styles.container}>
+                <Text style={styles.title}>피드</Text>
+            </View>
+            <View style={{flex:1}}>
+                <Story story={userState.otherStory} />
+                <Playlist playList={state.playlists} />
+            </View>       
+            <SelectedStory />
         </StoryProvider>
 
     );
 };
+
+const styles=StyleSheet.create({
+    container: {
+        width: '100%', 
+        height: (40 + StatusBarHeight) * tmpWidth, 
+        paddingTop: StatusBarHeight * tmpWidth,
+        backgroundColor: 'rgb(254,254,254)',
+    },
+    title: {
+        fontSize: 24 * tmpWidth,
+        fontWeight: '500',
+        marginLeft: 18 * tmpWidth
+    }
+})
 
 export default Feed;
