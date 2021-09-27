42661849355c3c5d53e09f989967b5250dde3357
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-09-28 00:54:54 +0900

    fix: unnecessary lines remove

--- src/components/Feed/Story.js
+++ src/components/Feed/Story.js
@@ -20,95 +20,57 @@ export default Story = ({ story }) => {
 
     return (
         <>
-        <View style={styles.container}>
-            <ScrollView
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{paddingLeft: 18 * tmpWidth}}
-            >
-                <View style={styles.nameArea}>
-                    { userState.myStory ? 
-                    <TouchableOpacity
-                        onPress={onClickMyStory}
-                        style={styles.story}
-                    >
-                        { userState.myStory.view.includes(userState.myInfo._id) ? 
-                        <ProfileImage img={userState.myInfo.profileImage} imgStyle={styles.profileImg}/> :
-                        <ImageBackground style={styles.story} source={require('assets/icons/profileStory.png')}>
+            <View style={styles.container}>
+                <ScrollView
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={{paddingLeft: 18 * tmpWidth}}
+                >
+                    <View style={styles.nameArea}>
+                        { userState.myStory ? 
+                        <TouchableOpacity
+                            onPress={onClickMyStory}
+                            style={styles.story}
+                        >
+                            { userState.myStory.view.includes(userState.myInfo._id) ? 
+                            <ProfileImage img={userState.myInfo.profileImage} imgStyle={styles.profileImg}/> :
+                            <ImageBackground style={styles.story} source={require('assets/icons/profileStory.png')}>
+                                <ProfileImage img={userState.myInfo.profileImage} imgStyle={styles.profileImg}/>
+                            </ImageBackground> }
+                        </TouchableOpacity> : 
+                        <TouchableOpacity
+                            style={styles.story}
+                            onPress={onClickNewStory}
+                        >
                             <ProfileImage img={userState.myInfo.profileImage} imgStyle={styles.profileImg}/>
-                        </ImageBackground> }
-                    </TouchableOpacity> : 
-                    <TouchableOpacity
-                        style={styles.story}
-                        onPress={onClickNewStory}
-                    >
-                        <ProfileImage img={userState.myInfo.profileImage} imgStyle={styles.profileImg}/>
-                        <SvgUri width={21} height={21} source={require('assets/icons/feedStoryPlus.svg')} style={styles.plus} />
-                    </TouchableOpacity> }
-                    <Text numberOfLines ={1} style={styles.name}>{userState.myInfo.name}</Text>
-                </View>
-                { story && story.map((item, index) => {
-                    const { song, profileImage, name } = item
-                    return (
-                        <View style={styles.nameArea} key={name}>
-                            <TouchableOpacity
-                                style={styles.story}
-                                onPress={() => onClickStory({ item, index })}
-                            >
-                                { song.view.includes(userState.myInfo._id) ? 
-                                <ProfileImage img={profileImage} imgStyle={styles.profileImg}/> : 
-                                <ImageBackground style={styles.story} source={require('assets/icons/profileStory.png')}>
-                                    <ProfileImage img={profileImage} imgStyle={styles.profileImg}/>
-                                </ImageBackground> }
-                            </TouchableOpacity>
-                            <Text numberOfLines ={1} style={styles.name}>{name}</Text>
-                        </View>
-                    )
-                })}
-            </ScrollView>
-            {/*
-            <FlatList
-                data={story}
-                keyExtractor={user=>user.id}
-                horizontal={true}
-                showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{paddingLeft: 27 * tmpWidth}}
-                renderItem={({item, index})=>{
-                    return (
-                        <View style={{height:115 * tmpWidth,marginRight:24 * tmpWidth,width:64 * tmpWidth}}>
-                            <View style={{alignItems: 'center'}}>
-                            {item.profileImage == undefined ?
-                                <TouchableOpacity onPress={() =>onClickStory({item, index})}>
-                                    {item.song.view.includes(userState.myInfo._id) ?
-                                    <View style={styles.storynopicread}>
-                                       <SvgUri width='100%' height='100%' source={require('../../assets/icons/noprofile.svg')} />
-                                    </View>  :
-                                    <ImageBackground style={{justifyContent:'center',alignItems:'center',width:tmpWidth*64, height:tmpWidth*64}} source={require('../../assets/icons/storylive.png')}>
-                                        <View style={styles.storynopicread}>
-                                           <SvgUri width='100%' height='100%' source={require('../../assets/icons/noprofile.svg')} />
-                                        </View> 
+                            <SvgUri width={21} height={21} source={require('assets/icons/feedStoryPlus.svg')} style={styles.plus} />
+                        </TouchableOpacity> }
+                        <Text numberOfLines ={1} style={styles.name}>{userState.myInfo.name}</Text>
+                    </View>
+                    { story && story.map((item, index) => {
+                        const { song, profileImage, name } = item
+                        return (
+                            <View style={styles.nameArea} key={name}>
+                                <TouchableOpacity
+                                    style={styles.story}
+                                    onPress={() => onClickStory({ item, index })}
+                                >
+                                    { song.view.includes(userState.myInfo._id) ? 
+                                    <ProfileImage img={profileImage} imgStyle={styles.profileImg}/> : 
+                                    <ImageBackground style={styles.story} source={require('assets/icons/profileStory.png')}>
+                                        <ProfileImage img={profileImage} imgStyle={styles.profileImg}/>
                                     </ImageBackground> }
-                                </TouchableOpacity> :
-                                <TouchableOpacity onPress={() => onClickStory({item, index})}>
-                                    {item.song.view.includes(userState.myInfo._id) ? <Image source={{uri: item.profileImage}} style={styles.storypicread}/> :
-                                    <ImageBackground style={{justifyContent:'center',alignItems:'center',width:tmpWidth*64, height:tmpWidth*64}} source={require('../../assets/icons/storylive.png')}>
-                                        <Image source={{uri: item.profileImage}} style={styles.storypic} />
-                                    </ImageBackground>
-                                    }
                                 </TouchableOpacity>
-                            }
-                                <Text numberOfLines ={1} style={{marginTop:6}}>{item.name}</Text>
+                                <Text numberOfLines ={1} style={styles.name}>{name}</Text>
                             </View>
-                        </View>
-                    )
-                }}
-            />
-            */}
-        </View>
-                    <SearchProvider>
-                    <NewStory newStory={newStory} setNewStory={setNewStory} />
-                </SearchProvider>
-                </>
+                        )
+                    })}
+                </ScrollView>
+            </View>
+            <SearchProvider>
+                <NewStory newStory={newStory} setNewStory={setNewStory} />
+            </SearchProvider>
+        </>
     )
 }
 
