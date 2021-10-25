7dc2e93b5dd575b12039d7a0fed03df717148238
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-25 15:02:10 +0900

    chore: eslint, prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['airbnb', 'prettier'],
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'no-use-before-define': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx'],
+    'import/resolver': {
+      'babel-module': {},
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  },
 };
