4a7ef144846bc4929c53181a2c042dcf9109f6f9
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-25 19:04:24 +0900

    refactor: code refactor with eslint in providers

--- src/providers/chat/index.js
+++ src/providers/chat/index.js
@@ -1,50 +1,47 @@
-import React, { createContext, useContext, useState, useRef } from 'react'
+import React, { createContext, useContext, useState, useRef } from 'react';
 import { Context as UserContext } from 'context/UserContext';
 import { Context as DJContext } from 'context/DJContext';
 import { navigate, push } from 'navigationRef';
 
-const ChatContext = createContext(null)
+const ChatContext = createContext(null);
 
-export const useChat = () => useContext(ChatContext)
+export const useChat = () => useContext(ChatContext);
 
-export default ChatProvider = ({ children }) => {
-    const { state: userState, getOtheruser } = useContext(UserContext);
-    const { getSongs } = useContext(DJContext);
-    const [text, setText] = useState(false)
-    const [optionModal, setOptionModal] = useState(false)
-    const textRef = useRef()
-    const chatRef = useRef()
+const ChatProvider = ({ children }) => {
+  const { state: userState, getOtheruser } = useContext(UserContext);
+  const { getSongs } = useContext(DJContext);
+  const [text, setText] = useState(false);
+  const [optionModal, setOptionModal] = useState(false);
+  const textRef = useRef();
+  const chatRef = useRef();
 
-    const onMove = () => {
-        if(chatRef) {
-            chatRef.current.scrollToEnd()        
-        }
+  const onMove = () => {
+    if (chatRef) {
+      chatRef.current.scrollToEnd();
     }
+  };
 
-    const onClickProfile = async (id) => {
-        if(id == userState.myInfo._id) {
-            navigate('Account')
-        }else{
-            await Promise.all([
-                getOtheruser({ id }),
-                getSongs({ id })
-            ]);
-            push('OtherAccount', {otherUserId: id})
-        }
+  const onClickProfile = async (id) => {
+    if (id === userState.myInfo._id) {
+      navigate('Account');
+    } else {
+      await Promise.all([getOtheruser({ id }), getSongs({ id })]);
+      push('OtherAccount', { otherUserId: id });
     }
+  };
 
-    const value = {
-        text,
-        textRef,
-        optionModal,
-        chatRef,
-        onMove,
-        setText,
-        setOptionModal,
-        onClickProfile,
-    }
+  const value = {
+    text,
+    textRef,
+    optionModal,
+    chatRef,
+    onMove,
+    setText,
+    setOptionModal,
+    onClickProfile,
+  };
 
-    return (
-        <ChatContext.Provider value={value}>{children}</ChatContext.Provider>
-    )
-}
+  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+};
+
+export default ChatProvider;
--- src/providers/modal/index.js
+++ src/providers/modal/index.js
@@ -1,23 +1,23 @@
-import React, { createContext, useContext, useState, useEffect } from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
-const ModalContext = createContext(null)
+const ModalContext = createContext(null);
 
-export const useModal = () => useContext(ModalContext)
+export const useModal = () => useContext(ModalContext);
 
-export default ModalProvider = ({ children }) => {
-    const [harmfulModal, setHarmfulModal] = useState(false)
+const ModalProvider = ({ children }) => {
+  const [harmfulModal, setHarmfulModal] = useState(false);
 
-    const onCloseModal = () => {
-        setHarmfulModal(false)
-    }
+  const onCloseModal = () => {
+    setHarmfulModal(false);
+  };
 
-    const value = {
-        harmfulModal,
-        setHarmfulModal,
-        onCloseModal
-    }
+  const value = {
+    harmfulModal,
+    setHarmfulModal,
+    onCloseModal,
+  };
 
-    return (
-        <ModalContext.Provider value={value}>{children}</ModalContext.Provider>
-    )
-}
+  return <ModalContext.Provider value={value}>{children}</ModalContext.Provider>;
+};
+
+export default ModalProvider;
