85430fd17a5d385f4f7e19f324fd167f9245e869
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-25 15:06:36 +0900

    chore: add rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
   rules: {
     'no-use-before-define': 'off',
     'prettier/prettier': 'error',
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'import/named': 0,
   },
   settings: {
     'import/extensions': ['.js', '.jsx'],
