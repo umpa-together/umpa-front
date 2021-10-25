51a6318dff9f0699df7d0bbd952460efc73c966c
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-25 19:05:05 +0900

    refactor: code refactor with eslint in Daily screens

--- src/screens/Daily/DailyCreate.js
+++ src/screens/Daily/DailyCreate.js
@@ -1,23 +1,25 @@
-import React, { useState }from 'react';
-import DailyProvider from 'providers/daily'
-import CreateMain from 'components/Daily/CreateMain'
-import SearchSong from 'components/Daily/SearchSong'
-import HarmfulModal from 'components/HarmfulModal'
+import React, { useState } from 'react';
+import DailyProvider from 'providers/daily';
+import CreateMain from 'components/Daily/CreateMain';
+import SearchSong from 'components/Daily/SearchSong';
+import HarmfulModal from 'components/HarmfulModal';
 
 const DailyCreatePage = ({ route }) => {
-    const { data: daily, isEdit } = route.params
-    const [isSearch, setIsSearch] = useState(false)
-    
-    return (
-        <>
-            <DailyProvider>
-                {!isSearch ? 
-                <CreateMain isEdit={isEdit} setIsSearch={setIsSearch} daily={daily} /> : 
-                <SearchSong isEdit={isEdit} setIsSearch={setIsSearch} daily={daily} /> }
-            </DailyProvider>
-            <HarmfulModal />
-        </>
-    );
+  const { data: daily, isEdit } = route.params;
+  const [isSearch, setIsSearch] = useState(false);
+
+  return (
+    <>
+      <DailyProvider>
+        {!isSearch ? (
+          <CreateMain isEdit={isEdit} setIsSearch={setIsSearch} daily={daily} />
+        ) : (
+          <SearchSong isEdit={isEdit} setIsSearch={setIsSearch} daily={daily} />
+        )}
+      </DailyProvider>
+      <HarmfulModal />
+    </>
+  );
 };
 
-export default DailyCreatePage ; 
+export default DailyCreatePage;
