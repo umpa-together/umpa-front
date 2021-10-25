ed36d44bb976e871ecc16599a959b4a57e630a74
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-10-25 19:05:27 +0900

    refactor: code refactor with eslint in Playlist screens

--- src/screens/Playlist/CreatePlayListPage.js
+++ src/screens/Playlist/CreatePlayListPage.js
@@ -1,23 +1,25 @@
-import React, { useState }from 'react';
-import PlaylistProvider from 'providers/playlist'
-import CreateMain from 'components/Playlist/CreateMain'
-import SearchSong from 'components/Playlist/SearchSong'
-import HarmfulModal from 'components/HarmfulModal'
+import React, { useState } from 'react';
+import PlaylistProvider from 'providers/playlist';
+import CreateMain from 'components/Playlist/CreateMain';
+import SearchSong from 'components/Playlist/SearchSong';
+import HarmfulModal from 'components/HarmfulModal';
 
 const PlaylistCreatePage = ({ route }) => {
-    const { data: playList, isEdit } = route.params
-    const [isSearch, setIsSearch] = useState(false)
-    
-    return (
-        <>
-            <PlaylistProvider>
-                {!isSearch ? 
-                <CreateMain isEdit={isEdit} setIsSearch={setIsSearch} playList={playList} /> : 
-                <SearchSong isEdit={isEdit} setIsSearch={setIsSearch} playList={playList} /> }
-            </PlaylistProvider>
-            <HarmfulModal />
-        </>
-    );
+  const { data: playList, isEdit } = route.params;
+  const [isSearch, setIsSearch] = useState(false);
+
+  return (
+    <>
+      <PlaylistProvider>
+        {!isSearch ? (
+          <CreateMain isEdit={isEdit} setIsSearch={setIsSearch} playList={playList} />
+        ) : (
+          <SearchSong isEdit={isEdit} setIsSearch={setIsSearch} playList={playList} />
+        )}
+      </PlaylistProvider>
+      <HarmfulModal />
+    </>
+  );
 };
 
-export default PlaylistCreatePage; 
+export default PlaylistCreatePage;
