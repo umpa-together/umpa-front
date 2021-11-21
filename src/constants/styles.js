da7cee38454bb9d763e8825dc968baae81374fd9
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2021-11-22 02:17:39 +0900

    feat: Add colors, styles constants

---
+++ src/constants/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  flexRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  space_between: {
+    justifyContent: 'space-between',
+  },
+});
+
+export default style;
