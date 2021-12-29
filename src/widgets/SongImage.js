426e003a1670d4a7b759f6cc1729c1bc8ed1edc1
Hyodori04 <x40807@gmail.com>
2021-12-29 23:56:37 +0900

    fix : image edited for android

--- src/widgets/SongImage.js
+++ src/widgets/SongImage.js
@@ -2,20 +2,10 @@ import React from 'react';
 import { Image, ImageBackground } from 'react-native';
 import { SCALE_WIDTH, SCALE_HEIGHT } from 'lib/utils/normalize';
 
-const SongImage = ({ size, border, url, opac = 1.0 }) => {
+const SongImage = ({ url, imgStyle }) => {
   url = url.replace('{w}', '300');
   url = url.replace('{h}', '300');
-  return (
-    <Image
-      style={{
-        height: size * SCALE_HEIGHT,
-        width: size * SCALE_WIDTH,
-        borderRadius: border * SCALE_HEIGHT,
-        opacity: opac,
-      }}
-      source={{ url }}
-    />
-  );
+  return <Image style={imgStyle} source={{ uri: url }} />;
 };
 
 const SongImageBack = ({ url, opac, width, height, border, imgStyle }) => {
