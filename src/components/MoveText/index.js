38fe17afdac0f3c4bcb7675ce3b5f4d8c1042f83
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2022-01-23 00:47:59 +0900

    fix: Change text's layout with center

--- src/components/MoveText/index.js
+++ src/components/MoveText/index.js
@@ -7,7 +7,7 @@ import { SCALE_WIDTH } from 'lib/utils/normalize';
 export default function MoveText({ container, text, isMove, isExplicit, textStyle }) {
   return (
     <View style={container}>
-      <View style={style.flexRow}>
+      <View style={[style.flexRow, styles.center]}>
         {isExplicit && <View style={styles.explicit} />}
         {isMove ? (
           <TextTicker duration={7000} bounce={false} marqueeDelay={1000} style={textStyle}>
@@ -29,5 +29,8 @@ const styles = StyleSheet.create({
     height: 12 * SCALE_WIDTH,
     borderWidth: 1 * SCALE_WIDTH,
     marginRight: 5 * SCALE_WIDTH,
+  },
+  center: {
+    justifyContent: 'center',
   },
 });
