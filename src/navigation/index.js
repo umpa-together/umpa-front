74cceb4a4ff858ea71e52e8fa24f51077862238f
Hyodori04 <x40807@gmail.com>
2022-02-16 21:11:51 +0900

    feat : firebase analytics

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import navigationRef from 'lib/utils/navigation';
 import { Context as AuthContext } from 'context/Auth';
 import { StatusBar } from 'react-native';
+import analytics from '@react-native-firebase/analytics';
 import MainStackScreen from './Main';
 import AuthStackScreen from './Auth';
 
 const MainNavigator = () => {
+  const routeNameRef = useRef();
+
   const {
     state: { token },
     tryLocalSignIn,
@@ -16,7 +19,24 @@ const MainNavigator = () => {
   }, []);
 
   return (
-    <NavigationContainer ref={navigationRef}>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}
+    >
       <StatusBar backgroundColor="transparent" translucent barStyle="dark-content" />
       {token ? <MainStackScreen /> : <AuthStackScreen />}
     </NavigationContainer>
