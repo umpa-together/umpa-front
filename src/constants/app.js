1332d85380e58f0f8f58467dc13a14f60ae8d4f4
Hyodori04 <x40807@gmail.com>
2022-02-18 18:14:28 +0900

    fix: social login, app Icon

--- src/constants/app.js
+++ src/constants/app.js
@@ -1,7 +1,7 @@
-export const kConsumerKey = 'Y8JPV5bG8TLUqZvfKDns';
-export const kConsumerSecret = 'LCSEiEcHah';
+export const kConsumerKey = 'so3Gjl6buzJ29rXRalJm';
+export const kConsumerSecret = 'hSEE4AQSqf';
 export const webClientIdIOS =
-  '217008100787-j3ohrpjsma12v7lkm6fnr6l9b14b4f7t.apps.googleusercontent.com';
+  '217008100787-fe5u8u3ejdp73d73rabbqefjctdgbvto.apps.googleusercontent.com';
 export const webClientIdAndroid =
   '217008100787-dmpqcjn1lj44u5jr124rhqaomhol8ic6.apps.googleusercontent.com';
 export const serverURL = 'http://52.78.209.215:3000';
