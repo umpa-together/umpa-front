fac2eaf2346cb35bc618d6243e840d3c1d183484
Hyodori04 <x40807@gmail.com>
2022-02-23 01:48:17 +0900

    fix : android build

--- src/constants/app.js
+++ src/constants/app.js
@@ -3,5 +3,5 @@ export const kConsumerSecret = 'hSEE4AQSqf';
 export const webClientIdIOS =
   '217008100787-fe5u8u3ejdp73d73rabbqefjctdgbvto.apps.googleusercontent.com';
 export const webClientIdAndroid =
-  '217008100787-dmpqcjn1lj44u5jr124rhqaomhol8ic6.apps.googleusercontent.com';
+  '217008100787-ks5140euqimnj1so0gi30g6fiqculad2.apps.googleusercontent.com';
 export const serverURL = 'http://52.78.209.215:3000';
